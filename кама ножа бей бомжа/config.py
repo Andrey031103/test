557e523d018ef6b8a76897bf92b726067f4bfec3
Andrey031103 <andreycroot@gmail.com>
2025-01-16 18:28:38 +0300

    Add files via upload

---
+++ кама ножа бей бомжа/config.py
@@ -0,0 +1,8 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
+bot_token = env('BOT_TOKEN')
+
+print(bot_token) 
---
+++ кама ножа бей бомжа/lexicon.py
@@ -0,0 +1,5 @@
+class Lexicon:
+    def __init__(self):
+        self.start = 'Привет! \n\n Давай сыграем в игру "Камень, ножницы, бумага"?'
+
+lexicon = Lexicon()
